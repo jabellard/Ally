2ca3cd9317e93f2aec0f146c3841ba57abdb30ed
Joe Nathan Abellard <contact@jabellard.com>
2020-04-11 09:58:33 -0400

    Implement ApplicationBuilder extensions

--- Ally.RabbitMQ/Extensions/ApplicationBuilderExtensions.cs
+++ Ally.RabbitMQ/Extensions/ApplicationBuilderExtensions.cs
@@ -1,7 +1,31 @@
+using Ally.RabbitMQ.Abstractions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace Ally.RabbitMQ.Extensions
 {
-    public class ApplicationBuilderExtensions
+    public static class ApplicationBuilderExtensions
     {
-        
+        public static IApplicationBuilder UseRabbitMq(this IApplicationBuilder builder)
+        {
+            var applicationLifetime = builder
+                .ApplicationServices.
+                GetService<IHostApplicationLifetime>();
+            
+            applicationLifetime.ApplicationStopping.Register(() =>
+            {
+                var rabbitMqConnections = builder
+                    .ApplicationServices.
+                    GetServices<IRabbitMqConnection>();
+                foreach (var rabbitMqConnection in rabbitMqConnections)
+                {
+                    rabbitMqConnection
+                        .Connection
+                        .Close();
+                }
+            });
+            return builder;
+        }
     }
 }
